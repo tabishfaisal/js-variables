d5985d8b4380aa85ce099923dc40f0f40acbb451
“tabihsfaisal“ <tabish.faisaltn@gmail.com>
2024-10-11 17:24:23 +0200

    solving other given equations

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,11 +1,25 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
+const Name = "Tabish"; // added my name as value of variable 
+console.log(Name); // using console log for variable Name to print on console
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
+const Number = 8; // Assegnato un numero
+const Text = "text"; // Assegnato una stringa
+const bool = false; // Assegnato una bool
+
+
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const number = 40; // Assegnato un numero
+console.log(number);// mando un console 
+const name  = "Tabish"; // Assegnato una stringa
+console.log(name);// console stringa
+const booleano = false; // Assegnato una bool
+console.log(booleano);//console booleano valore
+
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
@@ -14,7 +28,7 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = 'Jane '+'Smith' ;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
